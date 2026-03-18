748bf27d4bd03e385f28ee137f99428375fb5edf
Sakshi-Sharma11 <sakshii.sharmaa02@gmail.com>
2026-03-18 18:36:01 +0000

    Initial frontend commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <p style={{ textAlign: "center", fontWeight: 600 }}>
+        Sakshi Sharma - 25235137
+      </p>
+
+      {/* rest of your existing code below */}
+    </div>
+  );
+}
+
+export default App;
